880ff37871eb0fdd2dd530bb9cba4e2132f3f629
Akika <gemingjia0201@163.com>
2026-03-19 20:35:07 +0800

    init commit

---
+++ src/main/java/me/akika/github/MiniGithubApplication.java
@@ -0,0 +1,13 @@
+package me.akika.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniGithubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/akika/github/MiniGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package me.akika.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
