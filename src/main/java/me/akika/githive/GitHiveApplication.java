cdbcb980429659d16583da2279a14f9d67aa7e95
Akika <gemingjia0201@163.com>
2026-04-01 20:24:22 +0800

    build(auth): add MyBatis-Plus and auth configuration

--- src/main/java/me/akika/githive/GitHiveApplication.java
+++ src/main/java/me/akika/githive/GitHiveApplication.java
@@ -1,9 +1,13 @@
 package me.akika.githive;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@MapperScan("me.akika.githive.auth.mapper")
 public class GitHiveApplication {
 
     public static void main(String[] args) {
